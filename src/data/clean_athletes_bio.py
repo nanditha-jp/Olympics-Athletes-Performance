73493bf6dfa140d4a0cf07ba14cf07e0dbf10d4b
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-04-13 16:03:49 +0530

    add clean athletes bio

---
+++ src/data/clean_athletes_bio.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+
+import os
+import src.utils as utils
+
+
+# configure logger
+logger = utils.configure_logger("Cleaning Athletes Bio", "clean_bios.log")
+
+
+def clean_name(data: pd.DataFrame) -> pd.DataFrame:
+    try:
+        logger.debug("Cleaning name")
+        df = data.copy()
+        df["name"] = df["Used name"].str.replace("•", " ")
+        return df
+    except Exception as e:
+        logger.error("Error cleaning name: %s", e)
+        return pd.DataFrame()
+
+
+def extract_height_weight(data: pd.DataFrame) -> pd.DataFrame:
+    try:
+        logger.debug("Extracting height and weight")
+        df = data.copy()
+
+        # Split the Measurements column into height & weight columns
+        df[["height_cm", "weight_kg"]] = df["Measurements"].str.split("/", expand=True)
+
+        # Get rid of " cm" and the " kg" from our new columns
+        df["height_cm"] = pd.to_numeric(
+            df["height_cm"].str.strip(" cm"), errors="coerce"
+        )
+        df["weight_kg"] = pd.to_numeric(
+            df["weight_kg"].str.strip(" kg"), errors="coerce"
+        )
+
+        return df
+
+    except Exception as e:
+        logger.error("Error extracting height and weight: %s", e)
+        return pd.DataFrame()
+
+
+def extract_born_details(data: pd.DataFrame) -> pd.DataFrame:
+    try:
+        df = data.copy()
+
+        logger.debug("Extracting born date, year..")
+        # Parse out dates from 'Born' and 'Died' columns
+        date_pattern = r"(\d+ \w+ \d{4}|\d{4})"
+        df["born_date"] = df["Born"].str.extract(date_pattern)
+        df["born_year"] = df["Born"].str.extract(r"(\d{4})")
+
+        df["born_date"] = pd.to_datetime(
+            df["born_date"], format="mixed", errors="coerce"
+        )
+        df["born_year"] = pd.to_numeric(df["born_year"])
+
+        logger.debug("Extracting born location")
+        location_pattern = r"in ([\w\s()-]+), ([\w\s-]+) \((\w+)\)"
+        df[["born_city", "born_region", "born_country"]] = df["Born"].str.extract(
+            location_pattern, expand=True
+        )
+
+        return df
+
+    except Exception as e:
+        logger.error("Error extracting born details: %s", e)
+        return pd.DataFrame()
+
+
+def filter_columns(data: pd.DataFrame) -> pd.DataFrame:
+    try:
+        logger.debug("Filtering columns")
+        df = data.copy()
+        columns_to_remove = [
+            "Roles",
+            "Used name",
+            "Measurements",
+            "Born",
+            "Died",
+            "Affiliations",
+            "Nick/petnames",
+            "Title(s)",
+            "Other names",
+            "Nationality",
+            "Original name",
+            "Name order",
+        ]
+
+        return df.drop(columns=columns_to_remove)
+
+    except Exception as e:
+        logger.error("Error filtering columns: %s", e)
+        return pd.DataFrame()
+
+
+def rename_and_reorder(data: pd.DataFrame) -> pd.DataFrame:
+    try:
+        logger.debug("Renaming and reordering columns")
+        df = data.copy()
+
+        df = df.rename(
+            columns={
+                "Full name": "full_name",
+                "Sex": "sex",
+                "height_cm": "height",
+                "weight_kg": "weight",
+                "NOC": "noc",
+            }
+        )
+
+        columns_ordered = [
+            "athlete_id",
+            "full_name",
+            "name",
+            "sex",
+            "born_date",
+            "born_year",
+            "born_city",
+            "born_region",
+            "born_country",
+            "height",
+            "weight",
+            "noc",
+        ]
+
+        return df[columns_ordered]
+
+    except Exception as e:
+        logger.error("Error renaming and reordering columns: %s", e)
+        return pd.DataFrame()
+
+
+if __name__ == "__main__":
+
+    try:
+        # load data
+        data_path = os.path.join("data", "raw")
+        file_path = os.path.join(data_path, "bios.csv")
+
+        bios = utils.load_data(file_path, logger)
+        if bios.empty:
+            raise ValueError("Data is empty")
+
+        # clean data pipeline
+        bios = (
+            bios.pipe(clean_name)
+            .pipe(extract_height_weight)
+            .pipe(extract_born_details)
+            .pipe(filter_columns)
+            .pipe(rename_and_reorder)
+        )
+
+        # save data
+        data_path = os.path.join("data", "interim")
+        utils.save_data(bios, data_path, "bios.csv", logger)
+
+    except Exception as e:
+        logger.error("Error cleaning athletes: %s", e)
---
+++ src/utils.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+import os
+import logging
+
+def load_data(file_path: str, logger: logging) -> pd.DataFrame:
+    try:
+        logger.debug("Loading data from %s", file_path)
+        return pd.read_csv(file_path)
+    except Exception as e:
+        logger.error("Error loading data: %s", e)
+        return pd.DataFrame()
+
+
+def configure_logger(
+    name: str, log_file: str, log_level: str = "DEBUG"
+) -> logging.Logger:
+    # logging configure
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel("DEBUG")
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel("ERROR")
+
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+def save_data(
+    data: pd.DataFrame, data_path: str, file_name: str, logger: logging
+) -> None:
+    try:
+        logger.debug("Saving Data")
+        os.makedirs(data_path, exist_ok=True)
+
+        data.to_csv(os.path.join(data_path, file_name), index=False)
+        logger.info("Data saved successfully")
+    except Exception as e:
+        logger.error(f"Error saving data: {e}")
