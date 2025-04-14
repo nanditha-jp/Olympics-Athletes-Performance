27da124b1cde3ec0340249f63fe24474f8215381
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-04-14 12:59:39 +0530

    add page navigation

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+from utils import css
 
 
-st.header("Olympics Athlete Performance Analysis")
+with st.sidebar:
+    page = option_menu(
+        menu_title=None,
+        options=["Home"],
+        icons=["home"],
+        styles=css.get_menu_style()
+    )
+
+
+if page == "Home":
+    st.header("Olympics Athlete Performance Analysis")
---
+++ utils/css.py
@@ -0,0 +1,22 @@
+def get_menu_style():
+    return {
+        "container": {
+            "padding": "0!important",
+            "background-color": "#f5f6fa"
+        },
+        "icon": {
+            "color": "black",
+            "font-size": "16px"
+        },
+        "nav-link": {
+            "font-size": "14px",
+            "font-weight": "normal",  # removed extra semicolon
+            "color": "#2c3e50"
+        },
+        "nav-link-selected": {
+            "background-color": "#ff4b4b",
+            "color": "white",
+            "font-weight": "normal",
+            "border-radius": "8px"
+        }
+    }
